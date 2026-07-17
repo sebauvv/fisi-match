b4fbf40555740c50ba758bec667027b9162ff2a1
sebauvv <sebastian.castillo2@unmsm.edu.pe>
2026-07-17 10:01:48 -0500

    hotfix

--- backend/app/config.py
+++ backend/app/config.py
@@ -53,12 +53,13 @@ class Settings(BaseSettings):
     jwt_expire_minutes: int = os.getenv("JWT_EXPIRE_MINUTES")
 
     # CORS
-    cors_origins: list[str] = [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-    ]
+    # cors_origins: list[str] = [
+    #     "http://localhost:5173",
+    #     "http://localhost:5174",
+    #     "http://127.0.0.1:5173",
+    #     "http://127.0.0.1:5174",
+    # ]
+    cors_origins: list[str] = ["*"]
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
